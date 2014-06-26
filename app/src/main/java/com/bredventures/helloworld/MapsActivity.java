da388fd59394701e16947c1d49cf483f92044a64
TealShift <tealshift@gmail.com>
2014-06-26 03:59:10 -0700

    First commit

--- app/src/main/java/com/bredventures/helloworld/MapsActivity.java
+++ app/src/main/java/com/bredventures/helloworld/MapsActivity.java
@@ -11,7 +11,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
-        //A comment
+        //A thing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
